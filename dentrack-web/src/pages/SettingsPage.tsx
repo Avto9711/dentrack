f2372a4cfe1912c0e3df993cf38aca433583ea63
Angel Torres <avto9711@gmail.com>
2025-12-29 12:19:46 -0500

    changing env variable in dentrack web

--- dentrack-web/src/pages/SettingsPage.tsx
+++ dentrack-web/src/pages/SettingsPage.tsx
@@ -17,7 +17,7 @@ export function SettingsPage() {
           <h3>Variables requeridas</h3>
           <ul>
             <li><code>VITE_SUPABASE_URL</code></li>
-            <li><code>VITE_SUPABASE_ANON_KEY</code></li>
+            <li><code>VITE_SUPABASE_KEY</code></li>
           </ul>
           <p>Estas claves se comparten con la app móvil para garantizar una experiencia consistente.</p>
         </div>
